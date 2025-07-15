0c22c418d8b82859b3cf8a66a98c83f3db445570
Linda Barreto <barretolindageraldine@gmail.com>
2025-07-15 14:54:08 -0300

    Finalización de la sección de comentarios

---
+++ comentarios/firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDI6vKmTJ0kdXV_ik9LS34gGx7jA6NJVQ",
+  authDomain: "comentarios-grupo8.firebaseapp.com",
+  databaseURL: "https://comentarios-grupo8-default-rtdb.firebaseio.com",
+  projectId: "comentarios-grupo8",
+  storageBucket: "comentarios-grupo8.firebasestorage.app",
+  messagingSenderId: "602538460932",
+  appId: "1:602538460932:web:09c87cf83dbb50c6db7251",
+  measurementId: "G-S68T8ZE9NE"
+};
+
+firebase.initializeApp(firebaseConfig);
+
